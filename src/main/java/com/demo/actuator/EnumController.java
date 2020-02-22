69701cd08d29e2e4da5a389f36b180d530f29d41
meenakshisund <meenakshisundaraja@gmail.com>
2020-02-22 18:21:38 +0530

    Added ipAddress to check k8s LB

--- src/main/java/com/demo/actuator/EnumController.java
+++ src/main/java/com/demo/actuator/EnumController.java
@@ -4,7 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -12,12 +16,16 @@ import java.util.Map;
 public class EnumController {
 
     @GetMapping()
-    public Map displayAllDays(){
-        int i = 1;
-        Map<Integer, Enum<Weekdays>> map = new HashMap<>();
+    public Map displayAllDays() throws UnknownHostException {
+        List<String> days = new ArrayList<>();
         for(Weekdays item : Weekdays.values()){
-            map.put(i++, item);
+            days.add(item.name());
         }
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("ipAdress", InetAddress.getLocalHost().getHostAddress());
+        map.put("weekDays", days);
+
         return map;
     }
 }
